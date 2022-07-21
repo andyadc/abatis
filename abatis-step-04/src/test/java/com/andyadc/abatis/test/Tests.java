a8efa21e9cbcea4336a6ba9322b9556fc8fcd4e1
andyadc <andaicheng@gmail.com>
2022-07-21 17:54:00 +0800

    abatis

--- abatis-step-04/src/test/java/com/andyadc/abatis/test/Tests.java
+++ abatis-step-04/src/test/java/com/andyadc/abatis/test/Tests.java
@@ -1,22 +1,27 @@
 package com.andyadc.abatis.test;
 
+import com.andyadc.abatis.builder.xml.XMLConfigBuilder;
 import com.andyadc.abatis.io.Resources;
+import com.andyadc.abatis.session.Configuration;
 import com.andyadc.abatis.session.SqlSession;
 import com.andyadc.abatis.session.SqlSessionFactory;
 import com.andyadc.abatis.session.SqlSessionFactoryBuilder;
+import com.andyadc.abatis.session.defaults.DefaultSqlSession;
 import com.andyadc.abatis.test.entity.User;
 import com.andyadc.abatis.test.mapper.UserMapper;
 import com.andyadc.abatis.util.Utils;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.Reader;
 
 public class Tests {
 
     @Test
     public void test_SqlSessionFactory() throws IOException {
-        // 1. 从SqlSessionFactory中获取SqlSession
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mybatis-config.xml"));
+        // 1. 从 SqlSessionFactory中获取 SqlSession
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         // 2. 获取映射器对象
@@ -28,11 +33,18 @@ public class Tests {
     }
 
     @Test
-    public void test001() {
-        System.out.println(Utils.underline2CamelCase("user_login_id"));
-        System.out.println(Utils.underline2CamelCase("name"));
-        System.out.println(Utils.camelCase2Underline("Name"));
-        System.out.println(Utils.camelCase2Underline("UserName"));
-        System.out.println(Utils.camelCase2Underline("userName"));
+    public void test_selectOne() throws IOException {
+        // 解析 XML
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(reader);
+        Configuration configuration = xmlConfigBuilder.parse();
+
+        // 获取 DefaultSqlSession
+        SqlSession sqlSession = new DefaultSqlSession(configuration);
+
+        // 执行查询：默认是一个集合参数
+        Object[] req = {1L};
+        Object res = sqlSession.selectOne("com.andyadc.abatis.test.mapper.UserMapper.selectByUserId", req);
+        System.out.println(Utils.toJson(res));
     }
 }
